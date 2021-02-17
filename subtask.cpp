8ee0fb1a45fba0d7272eb10e0e955bf629df95aa
abhisekmaji <46377796+abhisekmaji@users.noreply.github.com>
2021-02-18 00:10:29 +0530

    added comments , changed colored to grayscale

--- subtask.cpp
+++ subtask.cpp
@@ -20,18 +20,22 @@ int main(int argc, char** argv){
         cout<<"Image not loaded"<<endl;
         return -1;
     }
+    
+    \\ converting the colored image to grayscale 
     Mat gray_img;
     cvtcolor(imsrc,gray_img,COLOR_BGR2GRAY);
     
     namedWindow("Win", 1);
     imshow("Win",gray_img);
 
-
+    \\projecting the grayscaled image to the required image 
     vector<Point2f> src_points;
     vector<Point2f> dst_points;
     dst_points.pushback(472,52);
     dst_points.pushback(472,832);
     dst_points.pushback(800,830);
     dst_points.pushback(800,52);
+    
+    return 0;
 
 }
