6938d54f22da6fe91584986041683638ee6c888d
abhisekmaji <46377796+abhisekmaji@users.noreply.github.com>
2021-02-19 01:13:12 +0530

    grayscale conversion done, projection done, cropping done
    need to resize the window created for better point selection

--- subtask.cpp
+++ subtask.cpp
@@ -12,7 +12,7 @@ vector<Point2f> src_points; // Global Variable
 void onMouseClick(int event, int x, int y, int flags, void* userdata)
 {
     //capture points if left button pressed wiht control key pressed
-    if( flags == (EVENT_FLAG_CTRLKEY + EVENT_FLAG_LBUTTON) ){
+    if( event == EVENT_LBUTTONDOWN ){
         src_points.push_back(Point2f(x,y));
         cout<< "coordinate- ("<< x <<","<< y << ")"<< endl;
     }
@@ -28,27 +28,30 @@ int main(int argc, char** argv){
         cout<<"Image not loaded"<<endl;
         return -1;
     }
-    
+
     // converting the colored image to grayscale 
     Mat gray_img;
     cvtColor(imsrc,gray_img,COLOR_BGR2GRAY);
     
-    namedWindow("Win", 1);
-    imshow("Win",gray_img);
-
+    namedWindow("Win", WINDOW_AUTOSIZE);
+    imshow("Win", gray_img);
+    
+    
     //projecting the grayscaled image to the required image 
-    //vector<Point2f> src_points;
+    
     
     //give the instructions to click the points
-    cout<<"click 4 points on Win in the following order and holding the CNTRL key"<<endl;
+    
+    cout<<"click 4 points on Win "<<endl;
     cout<<"top-left"<<endl;
     cout<<"bottom-left"<<endl;
     cout<<"bottom-right"<<endl;
     cout<<"top-right"<<endl;
     
-    while(src_points.size()<4){
-        setMouseCallback("Win",onMouseClick,0);
-    }
+
+    setMouseCallback("Win",onMouseClick,0);
+
+    waitKey(0);
     
     vector<Point2f> dst_points;
     dst_points.push_back(Point2f(472,52));
@@ -58,6 +61,7 @@ int main(int argc, char** argv){
     
     Mat homography = findHomography(src_points,dst_points);
     Mat im_out;
+
     warpPerspective(gray_img,im_out,homography,gray_img.size());
     
     //show the image projected
@@ -67,6 +71,7 @@ int main(int argc, char** argv){
     if(check1 == false){
         cout<<"Image could not be saved"<<endl;
     }
+    waitKey(0);
 
     //croping the image
     int top_left_x = 472;
@@ -82,7 +87,7 @@ int main(int argc, char** argv){
     if(check2 == false){
         cout<<"Image could not be saved"<<endl;
     }
-
+    
     waitKey(0);
 
     destroyAllWindows();    
