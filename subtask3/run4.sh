e221a8dc625488f6e075e8f91f8b3a6eefe1dc46
Lokesh Acharya <48222781+lokesh-acharya@users.noreply.github.com>
2021-03-31 22:56:08 +0530

    Update run4.sh

--- subtask3/run4.sh
+++ subtask3/run4.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Looping over method 4"
-g++ -o m4 m4.cpp -std=c++11 -lpthread `pkg-config --cflags --libs opencv`
+g++ -o m4 method4.cpp -std=c++11 -lpthread `pkg-config --cflags --libs opencv`
 g++ -o error error.cpp
 for i in 1 2 4 8 12 16 20 24 28 32
 do
